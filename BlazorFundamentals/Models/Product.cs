e4cf33c777cc9d8729a73bea4f6a31015c538a7d
Ant <Kozakanton@i.ua>
2025-12-09 21:24:07 +0100

    Blazor Fundamentals - Added Product model and LearnBlazor components

---
+++ BlazorFundamentals/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace BlazorFundamentals.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+    public IEnumerable<Product_Prop> ProductProperties { get; set; } = new List<Product_Prop>();
+}
