77abec59e584b5d5b48a66bd8fffa60270eb2e0d
Ant <Kozakanton@i.ua>
2025-12-16 14:29:36 +0100

    Blazor Forms Component Added

--- BlazorFundamentals/Models/Product.cs
+++ BlazorFundamentals/Models/Product.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorFundamentals.Models;
 
 public class Product
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
+    [Range(1, 100)]
     public double Price { get; set; }
     public bool IsActive { get; set; }
     public IEnumerable<Product_Prop> ProductProperties { get; set; } = new List<Product_Prop>();
+
+    public Category Category { get; set; }
+    public DateOnly AvailableFrom { get; set; }
+}
+
+
+public enum Category
+{
+    Entree, Appetizer, Dessert
 }
